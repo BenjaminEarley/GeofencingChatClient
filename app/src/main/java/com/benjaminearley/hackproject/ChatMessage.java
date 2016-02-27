ac8916e5cec14e07f424125d93fd179292be18c6
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-27 16:39:57 -0500

    added chat functionality

---
+++ app/src/main/java/com/benjaminearley/hackproject/ChatMessage.java
@@ -0,0 +1,78 @@
+package com.benjaminearley.hackproject;
+
+public class ChatMessage {
+
+    private String imageUrl;
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String gender;
+    private String message;
+
+    public ChatMessage() {
+    }
+
+
+    public ChatMessage(String message) {
+
+        this.message = message;
+    }
+
+    public ChatMessage(String imageUrl, String firstName, String lastName, String age, String gender, String message) {
+        this.imageUrl = imageUrl;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.message = message;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
--- app/src/main/java/com/benjaminearley/hackproject/MainActivityFragment.java
+++ app/src/main/java/com/benjaminearley/hackproject/MainActivityFragment.java
@@ -1,15 +1,26 @@
 package com.benjaminearley.hackproject;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.FirebaseListAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ListView listView;
+    private FirebaseListAdapter<ChatMessage> mListAdapter;
+    private EditText textEdit;
+    private Button sendButton;
 
     public MainActivityFragment() {
     }
@@ -17,6 +28,34 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        listView = (ListView) view.findViewById(R.id.list);
+        textEdit = (EditText) view.findViewById(R.id.text_edit);
+        sendButton = (Button) view.findViewById(R.id.send_button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textEdit.getText().toString();
+                ChatMessage message = new ChatMessage(text);
+                App.getFirebaseRef().push().setValue(message);
+                textEdit.setText("");
+            }
+        });
+
+        mListAdapter = new FirebaseListAdapter<ChatMessage>(getActivity(), ChatMessage.class,
+                android.R.layout.simple_list_item_1, App.getFirebaseRef()) {
+            @Override
+            protected void populateView(View view, ChatMessage chatMessage, int i) {
+                ((TextView) view.findViewById(android.R.id.text1)).setText(chatMessage.getMessage());
+            }
+
+        };
+
+        listView.setAdapter(mListAdapter);
+
+
+        return view;
     }
 }
