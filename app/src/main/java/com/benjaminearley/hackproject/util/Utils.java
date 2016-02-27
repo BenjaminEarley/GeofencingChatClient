82b9272390d1fd862da6aaff1b72f995ac433d13
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-27 14:18:03 -0500

    added circle util tool for bitmaps

---
+++ app/src/main/java/com/benjaminearley/hackproject/util/Utils.java
@@ -0,0 +1,22 @@
+package com.benjaminearley.hackproject.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+
+/**
+ * Created by bearley on 2/27/16.
+ */
+public class Utils {
+
+    public static Drawable makeCircleAvatar(Bitmap bmp) {
+        ShapeDrawable dr = new ShapeDrawable(new OvalShape());
+        dr.getPaint().setShader(new BitmapShader(bmp, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        dr.setIntrinsicWidth(bmp.getWidth());
+        dr.setIntrinsicHeight(bmp.getHeight());
+        return dr;
+    }
+}
