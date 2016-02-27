a74f093cd65e7c2ad90dbc714355ab52bb039c29
John Spaetzel <john.spaetzel@gmail.com>
2016-02-27 14:58:14 -0500

    Additional Camera dependencies

--- app/src/main/java/com/benjaminearley/hackproject/ProfileActivityFragment.java
+++ app/src/main/java/com/benjaminearley/hackproject/ProfileActivityFragment.java
@@ -1,5 +1,7 @@
 package com.benjaminearley.hackproject;
 
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,11 +9,19 @@ import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.benjaminearley.hackproject.util.CameraPreview;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ProfileActivityFragment extends Fragment {
+
+    private static final int REQUEST_CAMERA = 0;
 
     public ProfileActivityFragment() {
     }
@@ -25,6 +35,45 @@ public class ProfileActivityFragment extends Fragment {
                     .inflateTransition(R.transition.curve));
         }
 
+        if ( mayCaptureFromCamera() ) {
+            Camera mCamera;
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            CameraPreview mPreview = new CameraPreview(getContext(), mCamera);
+            FrameLayout preview = (FrameLayout) getActivity().findViewById(R.id.profile_image_button);
+            if ( preview != null ) {
+                preview.addView(mPreview);
+            }
+        }
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    private boolean mayCaptureFromCamera() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (getActivity().checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(CAMERA)) {
+            requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
+        } else {
+            requestPermissions(new String[]{CAMERA}, REQUEST_CAMERA);
+        }
+        return false;
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
