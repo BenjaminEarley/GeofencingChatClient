588ec626997dab3e19bbce376d4b5765baa5ec6a
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-26 22:12:50 -0500

    added some colors

--- app/src/main/java/com/benjaminearley/hackproject/PermissionActivity.java
+++ app/src/main/java/com/benjaminearley/hackproject/PermissionActivity.java
@@ -3,10 +3,12 @@ package com.benjaminearley.hackproject;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
@@ -18,6 +20,11 @@ public class PermissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorAccent));
+        }
 
         if (mayRequestLocation()) {
             Intent intent = new Intent(this, PermissionActivity.class);
