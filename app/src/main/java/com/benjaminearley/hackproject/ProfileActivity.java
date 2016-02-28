41ffc86f58f3ee3a3de48e060f3a94db085fbb86
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-28 02:18:52 -0500

    fixing orientation and adding gravitar option in menu

--- app/src/main/java/com/benjaminearley/hackproject/ProfileActivity.java
+++ app/src/main/java/com/benjaminearley/hackproject/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.benjaminearley.hackproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,6 +35,12 @@ public class ProfileActivity extends AppCompatActivity {
             case android.R.id.home:
                 super.onBackPressed();
                 break;
+            case R.id.action_gravitar:
+                String url = "https://en.gravatar.com/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                break;
             case R.id.action_logout:
                 App.getFirebaseRef().unauth();
                 SharedPreferencesUtil.clear(this);
@@ -41,6 +48,7 @@ public class ProfileActivity extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
                 finish();
+                break;
         }
         return true;
     }
