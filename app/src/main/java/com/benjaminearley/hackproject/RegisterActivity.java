9cfc19e879667942b983ca6a78cd57da7d288edc
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-27 21:50:46 -0500

    Partial Registration work done

--- app/src/main/java/com/benjaminearley/hackproject/RegisterActivity.java
+++ app/src/main/java/com/benjaminearley/hackproject/RegisterActivity.java
@@ -7,81 +7,37 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.benjaminearley.hackproject.util.SharedPreferencesUtil;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity {
 
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-    private TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
+        mLoginFormView = findViewById(R.id.register_form);
         mProgressView = findViewById(R.id.login_progress);
-
-        mRegisterTextView = (TextView) findViewById(R.id.email_sign_up_button);
-        mRegisterTextView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    private void attemptLogin() {
+    private void attemptRegister() {
 
         // Reset errors.
         mEmailView.setError(null);
@@ -131,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
             App.getFirebaseRef().authWithPassword(email, password, new Firebase.AuthResultHandler() {
                 @Override
                 public void onAuthenticated(AuthData authData) {
-                    SharedPreferencesUtil.setUserLogin(LoginActivity.this, email, password);
+                    SharedPreferencesUtil.setUserLogin(RegisterActivity.this, email, password);
                     onAuthentication(true, true);
                 }
 
@@ -154,10 +110,10 @@ public class LoginActivity extends AppCompatActivity {
         showProgress(false);
 
         if (success) {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-        } else if (wrongPassword){
+        } else if (wrongPassword) {
             mPasswordView.setError(getString(R.string.error_incorrect_password));
             mPasswordView.requestFocus();
         } else {
@@ -211,4 +167,5 @@ public class LoginActivity extends AppCompatActivity {
     public void onNeedRegistrationClick(View view) {
     }
 }
+
 
