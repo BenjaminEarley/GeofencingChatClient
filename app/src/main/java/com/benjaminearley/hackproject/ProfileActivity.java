1dfaa02f3b75476c04881793db5f92dee2ced876
Benjamin Earley <bearley@outsitenetworks.com>
2016-02-27 22:16:00 -0500

    Added logout button

--- app/src/main/java/com/benjaminearley/hackproject/ProfileActivity.java
+++ app/src/main/java/com/benjaminearley/hackproject/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.benjaminearley.hackproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -18,11 +20,24 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 super.onBackPressed();
                 break;
+            case R.id.action_logout:
+                App.getFirebaseRef().unauth();
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
         }
         return true;
     }
